5d030df83a846dda5b03d4992b3b55caa2402d0f
Vasya <vskesha@gmail.com>
2023-07-29 21:52:36 +0300

    setup.py modified

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 setup(
     name='clean_folder',
     version='0.0.1',
-    description='Sorts files in proper folder',
+    description='Sorts files in given folder',
     url='https://github.com/Vskesha/clean_folder.git',
     author='Vasyl Boliukh',
     author_email='vskesha@gmail.com',
